90951d8e73a475e4240591d50799623cca0a0172
Yash Deshpande <142728556+YashDeshpande29@users.noreply.github.com>
2024-08-05 00:17:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const path=require('path')
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"https://blogit-1-63di.onrender.com",credentials:true}))
 
 
 app.use('/api/auth',authRoute)
